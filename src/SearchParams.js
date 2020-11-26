271a0092e5b0bb033d93a6ea40d0bae372f19e93
sumaira367 <sumaira.nahmad@gmail.com>
2020-11-27 00:30:53 +0500

    async calls to Petfinder API, update Pet component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,43 @@
-import React, {useState} from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, {useState, useEffect} from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = ()=> {
     
      const [location, setLocation ]= useState("Seattle, WA");
      const [breeds, setBreeds] = useState([]);
      const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-     const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+     const [pets, setPets] = useState([]);
+
+     async function requestPets(){
+         const {animals} = await pet.animals({
+             location,
+             breed,
+             type: animal
+         })
+         setPets(animals || []);
+     }
+
+     useEffect(()=>{
+         setBreeds([]);
+         setBreed("");
+         pet.breeds(animal).then(({breeds: apiBreeds})=>{
+             const breedStrings = apiBreeds.map(({name})=>name);
+             setBreeds(breedStrings);
+         }, console.error);
+         //requestPets();
+     }, [animal, setBreeds, setBreed]);
 
     return (
         <div className="search-params">
             <h1>{location}</h1>
-            <form>
+            <form onSubmit = {(e) => {
+                e.preventDefault();
+                requestPets();
+
+            }}>
                 <label htmlFor="Location">
                     <input id="location" value={location} placeholder="Location"
                     onChange={e=>setLocation(e.target.value)}/>
@@ -21,6 +46,7 @@ const SearchParams = ()=> {
                 <BreedDropdown/>
                 <button>Submit</button>
             </form>
+            <Results pets={pets} />
            
         </div>
         
